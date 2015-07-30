42077e5d1104e41f78122f4de399e837fdb9e770
dylansteck <dylancsteck@gmail.com>
2015-07-30 21:01:57 +0000

    made a comment

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,5 +1,6 @@
 require "open-uri"
 class API
+	#this uses the ShopStyle API
 	def getbrands 
 		data = open("http://api.shopstyle.com/api/v2/retailers?pid=uid6609-30888897-90").read
 		JSON.parse(data)
